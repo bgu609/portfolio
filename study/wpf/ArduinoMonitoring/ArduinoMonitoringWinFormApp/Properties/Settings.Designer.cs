b9cdd0e3a52cdebc8b69d7e08380ccb3e00ccaba
bgu <giung609@naver.com>
2020-09-15 01:27:14 +0900

    복구 2

--- study/wpf/ArduinoMonitoring/ArduinoMonitoringWinFormApp/Properties/Settings.Designer.cs
+++ study/wpf/ArduinoMonitoring/ArduinoMonitoringWinFormApp/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ArduinoMornitoring.Properties
+namespace ArduinoMonitoringWinFormApp.Properties
 {
 
 
--- study/wpf/ArduinoMornitoring/Models/ShellModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArduinoMornitoring.Models
-{
-    public class ShellModel
-    {
-        public string ConnectTime { get; set; }
-        public string DisconnectTime { get; set; }
-        public bool ConnectMode { get; set; }
-    }
-}
